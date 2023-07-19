f31c3f0e2163c87f650910e5a1d24133b45e9018
dathan <dathanvp@gmail.com>
2023-07-19 11:26:12 -0700

    rename director too!

--- scripts/rename-repo.sh
+++ scripts/rename-repo.sh
@@ -5,3 +5,4 @@ REPO_NAME=`basename $(git rev-parse --show-toplevel)`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo $DIR
 find "$DIR/../" -name scripts -prune -o -name .git -prune -o -type f -exec sed -i ''  s/go-project-template/$REPO_NAME/g {} \; -print
+mv -v $DIR/../cmd/go-project-template $DIR/../cmd/$REPO_NAME
